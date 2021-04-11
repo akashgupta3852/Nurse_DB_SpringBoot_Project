cdbc8cb968300c547cf7b54bd1f7da021a20e28f
Akash Gupta <akashgupta3852@gmail.com>
2021-04-11 14:27:25 +0530

    Akash[REFACTORED]: Modified the way of creating nurse data in database using dependecy injection @Autowired

--- src/main/java/com/example/demo/SpringbootrestdemoApplication.java
+++ src/main/java/com/example/demo/SpringbootrestdemoApplication.java
@@ -4,9 +4,10 @@ import java.util.Collections;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import com.example.demo.model.Nurse;
@@ -21,20 +22,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class SpringbootrestdemoApplication {
+public class SpringbootrestdemoApplication implements CommandLineRunner {
 	private static Logger logger = LoggerFactory.getLogger(SpringbootrestdemoApplication.class);
 
-	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(SpringbootrestdemoApplication.class, args);
+	@Autowired
+	private NurseService nurseService;
 
-		// It is used to create a nurse's data in the database
-		Nurse nurse = new Nurse();
-		nurse.setAge(31);
-		nurse.setName("Rama");
-		nurse.setSalary(71000);
-		nurse.setEmail("rama@gmail.com");
-		NurseService nurseService = applicationContext.getBean(NurseService.class);
-		nurseService.addNurse(nurse);
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestdemoApplication.class, args);
 
 		logger.info("Message logged at info level");
 		logger.error("Message logged at error level");
@@ -54,6 +49,18 @@ public class SpringbootrestdemoApplication {
 		return new ApiInfo("Nurse API", "Simple APIs for crud operation on Nurse", "1.0", "Free to use",
 				new Contact("Akash Gupta", "http://capgemini.com", "akash.a.gupta@capgemini.com"), "API License",
 				"http://capgemini.com", Collections.emptyList());
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// It is used to create a nurse's data in the database
+		Nurse nurse = new Nurse();
+		nurse.setAge(31);
+		nurse.setName("Rama");
+		nurse.setSalary(71000);
+		nurse.setEmail("rama@gmail.com");
+		nurseService.addNurse(nurse);
+		System.out.println("The nurse's data has been added to db");
 	}
 
 }
