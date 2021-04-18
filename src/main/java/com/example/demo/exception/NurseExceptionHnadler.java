87d244f0ef7d194495f8852ac03a99e1d65fff79
Akash Gupta <akashgupta3852@gmail.com>
2021-04-18 21:19:39 +0530

    Akash[ADDED]: Method for handling the date format exception is added

--- src/main/java/com/example/demo/exception/NurseExceptionHnadler.java
+++ src/main/java/com/example/demo/exception/NurseExceptionHnadler.java
@@ -1,10 +1,12 @@
 package com.example.demo.exception;
 
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,4 +21,9 @@ public class NurseExceptionHnadler {
 		return new ResponseEntity<String>("Exception while processing REST Requests" + errMsg, HttpStatus.BAD_REQUEST);
 	}
 
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<String> handle(HttpMessageNotReadableException exception) {
+		String error = "Exception while processing REST Requests, startDate should be in the dd MMM yyyy format.";
+		return new ResponseEntity<String>(error, HttpStatus.BAD_REQUEST);
+	}
 }
