4ab83b9e9160b01ffc71c4e7e5614e771046f47e
Akash Gupta <akashgupta3852@gmail.com>
2021-04-18 19:58:53 +0530

    Akash[ADDED]: Added the exception layer to handle the exceptions raised by client at time of passing the arguments

---
+++ src/main/java/com/example/demo/exception/NurseExceptionHnadler.java
@@ -0,0 +1,22 @@
+package com.example.demo.exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class NurseExceptionHnadler {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMsg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
+		return new ResponseEntity<String>("Exception while processing REST Requests" + errMsg, HttpStatus.BAD_REQUEST);
+	}
+
+}
